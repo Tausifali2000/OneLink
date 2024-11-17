9777ea75f775f56bafcbc08ca4ed92339f45a087
TausifAli2000 <tausifali2000@gmail.com>
2024-11-17 17:24:04 +0530

    filechanges

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Header from "./components/header/Header"
-import Home from "./pages/home/home"
+
 import Games from "./pages/games/Games"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Anime from "./pages/anime/Anime"
 import Entertainment from "./pages/entertainment/Entertainment"
 import Softwares from "./pages/softwares/Softwares"
+import Home from "./pages/home/home"
 
 
 const router = createBrowserRouter(
@@ -13,7 +14,7 @@ const router = createBrowserRouter(
       element: 
       <div>
         <Header />
-        <Home />
+        <Home/>
       </div>
       
     },
